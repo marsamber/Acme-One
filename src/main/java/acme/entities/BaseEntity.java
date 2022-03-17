60cf0d4565aed4b61f89d52cd8a2d43b2b32c06d
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2022-03-17 17:50:03 +0100

    fix type money in baseEntity

--- src/main/java/acme/entities/BaseEntity.java
+++ src/main/java/acme/entities/BaseEntity.java
@@ -2,13 +2,14 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.validation.constraints.Min;
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,32 +19,36 @@ import lombok.Setter;
 @Setter
 public class BaseEntity extends AbstractEntity {
 
-		//Serialisation identifier ------------------------------
+	//Serialisation identifier ------------------------------
+
+	protected static final long serialVersionUID = 1L;
 	
-		protected static final long serialVersionUID = 1L;
+	//Attributes ---------------------------------------------
+	
+	@NotBlank
+	@Length(max = 100)
+	protected String name;
+	
+	@Column(unique = true)
+	@Pattern(regexp ="^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	protected String code;
+	
+	@NotBlank
+	@Length(max = 100)
+	protected String technology;
+	
+	@NotBlank
+	@Length(max = 255)
+	protected String description;
+	
+	protected Money retailPrice;
+	
+	@URL
+	protected String link;
 		
-		//Attributes ---------------------------------------------
 		
-		@NotBlank
-		@Length(max = 100)
-		protected String name;
-		
-		@Column(unique = true)
-		@Pattern(regexp ="^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
-		protected String code;
-		
-		@NotBlank
-		@Length(max = 100)
-		protected String technology;
-		
-		@NotBlank
-		@Length(max = 255)
-		protected String description;
-		
-		@Min(1)
-		protected Double retailPrice;
-		
-		@URL
-		protected String link;
-		
+	@AssertTrue(message="Retail price must be positive")
+	private boolean isMoneyPositive() {
+		return this.retailPrice.getAmount() > 0.;
+	}
 }
