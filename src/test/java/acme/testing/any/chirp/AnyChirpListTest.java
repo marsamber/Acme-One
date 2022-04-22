b6531a5e4d36b073d265c26df4b8a6eef262234d
marcasbon <marcasbon@alum.us.es>
2022-04-22 19:30:41 +0200

    Test

---
+++ src/test/java/acme/testing/any/chirp/AnyChirpListTest.java
@@ -0,0 +1,48 @@
+/*
+ * FavouriteLinkTest.java
+ *
+ * Copyright (C) 2012-2022 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.testing.any.chirp;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AnyChirpListTest extends TestHarness{
+	
+
+	// Lifecycle management ---------------------------------------------------
+
+	// Test cases -------------------------------------------------------------
+
+
+	@ParameterizedTest	
+	@CsvFileSource(resources = "/any/chirp/list-chirp.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTestAdministratorChirp(final int recordIndex, final String creationMoment, final String title, final String author, final String body, final String email) {
+
+		super.signIn("administrator", "administrator");
+		super.clickOnMenu("Any", "List Chirps");
+		super.checkColumnHasValue(recordIndex, 0, creationMoment);
+		super.checkColumnHasValue(recordIndex, 1, title);
+		super.checkColumnHasValue(recordIndex, 2, author);
+		super.checkColumnHasValue(recordIndex, 3, body);
+		super.checkColumnHasValue(recordIndex, 4, email);
+		super.signOut();
+	}
+} 
+	
+
+
+	// Ancillary methods ------------------------------------------------------
+
