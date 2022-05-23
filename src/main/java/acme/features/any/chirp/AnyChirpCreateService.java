6982274e1f9349a5d409a1ca45fa9b9b1ea0336f
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2022-05-23 11:24:38 +0200

    Add spam

--- src/main/java/acme/features/any/chirp/AnyChirpCreateService.java
+++ src/main/java/acme/features/any/chirp/AnyChirpCreateService.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.Chirp;
+import acme.entities.SystemConfiguration;
+import acme.features.antiSpam.SpamDetector;
+import acme.features.antiSpam.SpamDetectorRepository;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Errors;
 import acme.framework.controllers.Request;
@@ -17,6 +20,9 @@ public class AnyChirpCreateService implements AbstractCreateService<Any, Chirp> 
 	
 	@Autowired
 	protected AnyChirpRepository repository;
+	
+	@Autowired
+	protected SpamDetectorRepository repositorySpam; 
 
 	@Override
 	public boolean authorise(final Request<Chirp> request) {
@@ -64,6 +70,17 @@ public class AnyChirpCreateService implements AbstractCreateService<Any, Chirp> 
 
 		confirmation = request.getModel().getBoolean("confirmation");
 		errors.state(request, confirmation, "confirmation", "any.Chirp.confirmation.error");
+		
+		final SystemConfiguration systemConfiguration= this.repositorySpam.findTheSystemConfiguration();
+        final SpamDetector spamDetector= new SpamDetector(systemConfiguration);
+
+        errors.state(request, !spamDetector.detectSpam(entity.getAuthor()), "author", "any.chirp.form.error.spam");
+ 
+        errors.state(request, !spamDetector.detectSpam(entity.getBody()), "body", "any.chirp.form.error.spam");
+        
+        errors.state(request, !spamDetector.detectSpam(entity.getEmail()), "email", "any.chirp.form.error.spam");
+        
+        errors.state(request, !spamDetector.detectSpam(entity.getTitle()), "title", "any.chirp.form.error.spam");
 		
 	}
 
