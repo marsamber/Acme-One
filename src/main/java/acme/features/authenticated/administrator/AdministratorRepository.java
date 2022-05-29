c8b49596e051568f2ef33b13ee6bd6683ddf40d1
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2022-05-29 14:11:39 +0200

    Fix dashboard

---
+++ src/main/java/acme/features/authenticated/administrator/AdministratorRepository.java
@@ -0,0 +1,33 @@
+/*
+ * AuthenticatedProviderRepository.java
+ *
+ * Copyright (C) 2012-2022 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.authenticated.administrator;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.framework.entities.UserAccount;
+import acme.framework.repositories.AbstractRepository;
+import acme.framework.roles.Administrator;
+
+@Repository
+public interface AdministratorRepository extends AbstractRepository {
+
+	@Query("select a from Administrator a where a.userAccount.id = :id")
+	Administrator findOneProviderByUserAccountId(int id);
+
+	@Query("select ua from UserAccount ua where ua.id = :id")
+	UserAccount findOneUserAccountById(int id);
+	
+	
+
+}
